ef871f1e559dad91a927f85869169697eb5174db
Derek Argueta <darguetap@gmail.com>
2015-09-19 08:17:39 -0400

    random stuff

--- mobile/src/main/java/co/jola/jola/TextClassifier.java
+++ mobile/src/main/java/co/jola/jola/TextClassifier.java
@@ -3,6 +3,9 @@ package co.jola.jola;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 
 import com.firebase.client.Firebase;
@@ -21,23 +24,32 @@ public class TextClassifier {
     private Context appContext;
     private Intent returnAction;
     private Firebase ref;
+    private SpeechRecognizer sr;
+    private TextToSpeech jola;
+    private Intent intent;
 
-    public TextClassifier(Context appContext) {
+    public TextClassifier(Context appContext, SpeechRecognizer sr, TextToSpeech jola) {
         this.appContext = appContext;
+        this.sr = sr;
+        this.jola = jola;
         this.returnAction = null;
         Firebase.setAndroidContext(appContext);
         ref = new Firebase("https://jola.firebaseio.com/");
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, "co.jola.jola");
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 0);
     }
 
     public void parseText(String inputText) {
-        if(inputText.contains("uber")) {
+        if(UberAPI.UberMatch(inputText)) {
             Log.d(TAG, "Uber is called");
             // TODO - call Uber API
-            ref.child("yelp").setValue();
-            returnAction = new UberAPI().openTest(appContext);
-        }
-        else {
-            HashSet<String> keys = new HashSet<String>();
+            UberAPI uber = new UberAPI(appContext);
+
+            returnAction = uber.execute();
+        } else {
+            HashSet<String> keys = new HashSet<>();
             keys.add("food");
             keys.add("eat");
             keys.add("dinner");
--- mobile/src/main/java/co/jola/jola/apis/BraintreeAPI.java
+++ mobile/src/main/java/co/jola/jola/apis/BraintreeAPI.java
@@ -1,8 +1,18 @@
 package co.jola.jola.apis;
 
+import android.content.Intent;
+
 /**
  * Created by jasonlin on 9/19/15.
  */
-public class BraintreeAPI {
-    // TODO - integrate Venmo API
+public class BraintreeAPI implements API {
+    // TODO - integrate Braintree API
+
+    public static boolean BraintreeMatch(String input) {
+        return input.contains("pay");
+    }
+
+    public Intent execute() {
+        return new Intent();
+    }
 }
