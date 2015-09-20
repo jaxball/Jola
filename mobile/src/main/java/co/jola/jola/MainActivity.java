663e93510a482e698fb928ef6a96b01b147934d5
Derek Argueta <darguetap@gmail.com>
2015-09-20 03:00:13 -0400

    move button to right

--- mobile/src/main/java/co/jola/jola/MainActivity.java
+++ mobile/src/main/java/co/jola/jola/MainActivity.java
@@ -2,10 +2,8 @@ package co.jola.jola;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Build;
